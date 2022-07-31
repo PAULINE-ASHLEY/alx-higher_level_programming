c20049798d168ba59b24c533baecf6c99c06804c
PAULINE-ASHLEY <37777577+PAULINE-ASHLEY@users.noreply.github.com>
2022-07-31 16:39:09 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,4 +10,8 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = int(size)
+            self.__size = size
+
+    def area(self):
+        """ Method that returns the square area of the object"""
+        return (self.__size ** 2)
