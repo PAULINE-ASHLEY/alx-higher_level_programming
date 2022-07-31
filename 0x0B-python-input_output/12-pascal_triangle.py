6e5b6a0ecb73eb97bb6a346093115f59e1b1c010
PAULINE-ASHLEY <37777577+PAULINE-ASHLEY@users.noreply.github.com>
2022-07-31 16:22:03 +0300

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-"""Module 12-pascal_triangle.
-Returns a list of lists of integers
-representing the Pascal’s triangle of n.
+""" Function that returns the pascal triangle
+    Args:
+        n: number of lines
+    Returns:
+        Empty list: if n <= 0 Otherwise pascal triangle
 """
 
 
 def pascal_triangle(n):
-    """Returns the pascal triangle of n.
-    Args:
-        - n: size of the triangle (rows)
-    Returns: a list of list of integers
-    """
 
     if n <= 0:
-        return []
+        return ([])
 
-    l = [[0 for x in range(i + 1)] for i in range(n)]
-    l[0] = [1]
-
+    pascal_t = [[1]]
     for i in range(1, n):
-        l[i][0] = 1
-        for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
-            else:
-                l[i][j] = l[i - 1][0]
-    return l
+        row = [1]
+        prev = pascal_t[i - 1]
+        for j in range(len(prev)):
+                new = prev[j] + prev[j + 1] if j != len(prev) - 1 else 1
+                row.append(new)
+
+        pascal_t.append(row)
+
+    return pascal_t
