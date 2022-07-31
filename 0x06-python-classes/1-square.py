3169c09d80ffa7ed4797b35d32bcc78f88dae421
PAULINE-ASHLEY <37777577+PAULINE-ASHLEY@users.noreply.github.com>
2022-07-31 16:36:35 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with size (no type/value verification).
-    """
 
+"""class Square: Create a square class that  contains a size
+"""
+
+
+class Square:
+    """Class that defines a square with size"""
     def __init__(self, size):
-        """Initializes the data."""
+        """Initialize method that stores the size of the square"""
         self.__size = size
