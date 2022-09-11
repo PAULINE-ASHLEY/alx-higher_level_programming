8368a6f56f8673a4d9df4bee47c7eca30d03615b
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-09-11 15:13:38 +0000

    creation of the files

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-
-const count = parseInt(process.argv[2]);
-
-// Check if argument is NaN
-
-if (isNaN(count)) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
 	  console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < count; i++) {
+	  const x = Number(process.argv[2]);
+	  let i = 0;
+	  while (i < x) {
 		      console.log('C is fun');
+		      i++;
 		    }
 }
